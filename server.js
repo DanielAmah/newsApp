014bcbd3a41b754a43f7a39a3d97edced5aeb69c
DanielAmah <amahdanieljack@gmail.com>
2017-06-04 12:59:54 +0100

    add server.js

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ module.exports = {
   app: function () {
     const app = express()
     const indexPath = path.join(__dirname, 'index.html')
-    const publicPath = express.static(path.join(__dirname, 'dist'))
+    const publicPath = express.static(path.join(__dirname, 'public'))
 
-    app.use('/dist', publicPath)
+    app.use('/public', publicPath)
     app.get('/', function (_, res) { res.sendFile(indexPath) })
 
     return app
